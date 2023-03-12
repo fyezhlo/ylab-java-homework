d60dff33b213cba454127eb1789c6130ae89ed61
Fyodor <fyezhlo@gmail.com>
2023-03-12 15:32:25 +0300

    printer fix

--- src/main/java/com/ylab/homework/hw2/printer/RateLimitedPrinterTest.java
+++ src/main/java/com/ylab/homework/hw2/printer/RateLimitedPrinterTest.java
@@ -4,7 +4,7 @@ import java.time.Instant;
 
 public class RateLimitedPrinterTest {
     public static void main(String[] args) {
-        RateLimitedPrinter rateLimitedPrinter = new RateLimitedPrinter(1000);
+        RateLimitedPrinter rateLimitedPrinter = new RateLimitedPrinterImpl(1000);
 
         for (int i = 0; i < 1_000_000_000; i++) {
             rateLimitedPrinter.print(Instant.now().toString());
