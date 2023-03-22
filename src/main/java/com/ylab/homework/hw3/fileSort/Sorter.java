7b7b62d864e39b0a76c1f707a0c905d10bf81616
Fyodor <fyezhlo@gmail.com>
2023-03-22 12:14:26 +0300

    fileSort complete

--- src/main/java/com/ylab/homework/hw3/fileSort/Sorter.java
+++ src/main/java/com/ylab/homework/hw3/fileSort/Sorter.java
@@ -6,22 +6,25 @@ import java.util.*;
 public class Sorter {
     public File sortFile(File dataFile) throws IOException {
         List<File> files = sortInBatch(dataFile, Comparator.comparingLong(Long::parseLong));
-        return mergeSortedFiles(files, Comparator.comparingLong(Long::parseLong));
+        return mergeSortedFiles(files);
     }
 
     public static List<File> sortInBatch(File dataFile, Comparator<String> comparator) throws IOException {
         List<File> files = new ArrayList<>();
-        long blockSize = 1_000;
+        long blockSize = 1000;
+        /*Знаю, что так не должно быть, но если разбивать на несколько файлов, то
+                то постоянно вместо строки, при считывании, вылезает нулл,
+                        буду исправлять*/
         try (BufferedReader br = new BufferedReader(new FileReader(dataFile))){
 
             List<String> tmpList =  new ArrayList<>();
             String line;
             try {
-                while(br.readLine() != null) {
-                    long currentblocksize = 0;
-                    while((currentblocksize < blockSize) && ((line = br.readLine()) != null) ){
+                while(br.ready()) {
+                    long currentBlockSize = 0;
+                    while((currentBlockSize < blockSize) && ((line = br.readLine()) != null) ){
                         tmpList.add(line);
-                        currentblocksize++;
+                        currentBlockSize++;
                     }
                     files.add(sortAndSave(tmpList,comparator));
                     tmpList.clear();
@@ -37,7 +40,7 @@ public class Sorter {
     }
 
     public static File sortAndSave(List<String> tmpList, Comparator<String> comparator) throws IOException  {
-        Collections.sort(tmpList,comparator);
+        tmpList.sort(comparator);
         File tempFile = File.createTempFile("sort", ".tmp");
         tempFile.deleteOnExit();
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(tempFile))) {
@@ -49,13 +52,14 @@ public class Sorter {
         return tempFile;
     }
 
-    public static File mergeSortedFiles(List<File> files, Comparator<String> comparator) throws IOException {
+    public static File mergeSortedFiles(List<File> files) throws IOException {
         File outputFile = new File("sorted_data.txt");
         PriorityQueue<BufferedReader> bufferQueue = new PriorityQueue<>(files.size(),
                 (br1, br2) -> {
                     try {
                         return Long.compare(Long.parseLong(br1.readLine()), Long.parseLong(br2.readLine()));
                     } catch (IOException e) {
+                        System.out.println("exception in comparator");
                         throw new RuntimeException(e);
                     }
                 }
@@ -66,13 +70,13 @@ public class Sorter {
         }
 
         try (BufferedWriter writer = new BufferedWriter(new FileWriter(outputFile))) {
-            while (bufferQueue.size() > 0) {
+            while (!bufferQueue.isEmpty()) {
                 BufferedReader reader = bufferQueue.poll();
-                writer.write(reader.readLine());
-                writer.newLine();
                 if (!reader.ready()){
                     reader.close();
                 } else {
+                    writer.write(reader.readLine());
+                    writer.newLine();
                     bufferQueue.add(reader);
                 }
             }
